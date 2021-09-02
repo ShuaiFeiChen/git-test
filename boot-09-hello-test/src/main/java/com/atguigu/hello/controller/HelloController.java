9d876ca6548c2f3956a6363deeec585a35ba96a0
ShuaiFeiChen <chenshuaifei163@163.com>
2021-09-02 22:58:19 +0800

    将本地工程添加到git

---
+++ boot-09-hello-test/src/main/java/com/atguigu/hello/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.atguigu.hello.controller;
+
+import com.atguigu.hello.service.HelloService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HelloController {
+
+    @Value("${spring.mvc.format.date-time:yyyy-MM-dd HH:mm:ss}")
+    private String dateTimeFormate;
+
+    @Resource
+    private HelloService helloService;
+
+    @GetMapping("/hello")
+    public String hello(String userName){
+        System.out.println(dateTimeFormate);
+        return helloService.sayHello(userName);
+    }
+
+}
